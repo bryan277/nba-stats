571b567907485fea06b9dccc0ed044c33991e0e1
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-12 15:41:03 -0800

    The input performs an onChange function call. Does nothing as but console log the change for now.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      searchInput: "thank you"
+    }
+  }
+
+  search(event){
+    console.log(event.target.value)
+  }
+
   render(){
     return(
-      <div>
-
+      <div className="search-container">
+        <input type="text" className="search" onChange={this.search} />
       </div>
     )
   }
