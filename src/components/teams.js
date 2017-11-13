aa3a7ffec171deb0a57a05eaed8f0bc8bc21ab3b
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-12 18:03:21 -0800

    Render all images

--- src/components/teams.js
+++ src/components/teams.js
@@ -4,7 +4,14 @@ class Teams extends Component {
   constructor(props){
     super(props);
     this.state = {
-      teams: ["warriors", "bucks"]
+      teams: [
+        "76ers", "blazers", "bucks", "bulls", "cavaliers",
+        "celtics", "clippers", "grizzlies", "hawks", "heat",
+        "hornets", "jazz", "kings", "knicks", "lakers",
+        "magic", "mavericks", "nets", "nuggets", "pacers",
+        "pelicans", "pistons", "raptors", "rockets", "spurs",
+        "suns", "thunder", "timberwolves", "warriors", "wizards"
+       ]
     }
   }
 
@@ -12,7 +19,7 @@ class Teams extends Component {
     return(
       <div className="teams-container">
         { this.state.teams.map((team, index) =>
-          <div className="team" key={index}><img src={`/images/teams/${team}.png`} /></div>
+          <div className="team" key={index}><img src={`/images/teams/${team}.png`} alt={team} /></div>
         )}
       </div>
     )
