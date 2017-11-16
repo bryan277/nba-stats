66a0ae1aa2f1b34fdbf8102da63cde2efc1703b2
Edgar Barajas <edgarsbarajas@yahoo.com>
2017-11-16 14:56:09 -0800

    Add team logo to roster page

--- src/components/roster.js
+++ src/components/roster.js
@@ -29,112 +29,117 @@ class Roster extends Component {
   render(){
     return(
       <div className="roster-container">
-        {this.state.roster.map((player) =>
-          <div className="player-container" key={player.id} onClick={(e) => {this.toggleExtendedData(e)}}>
-            <div className="headshot">
-              <img src={`/images/player.png`} alt="headshot"/>
+        <div className="team-logo">
+          <img src={`/images/teams/${this.state.team}.png`} alt="logo" />
+        </div>
+        <div className="players">
+          {this.state.roster.map((player) =>
+            <div className="player-container" key={player.id} onClick={(e) => {this.toggleExtendedData(e)}}>
+              <div className="headshot">
+                <img src={`/images/player.png`} alt="headshot"/>
+              </div>
+              <div className="simple-data-container">
+                <div className="name-position-container">
+                  <div className="name">
+                    {player.name}
+                  </div>
+                  <div className="position">
+                    {player.position}
+                  </div>
+                </div>
+                <div className="jersey-number">
+                  {player.jersey_number}
+                </div>
+              </div>
+              <div className="expanded-data-container hide">
+                <div className="bio-container">
+                  <div className="expanded-data-item height">
+                    <div className="edi-attribute">
+                      Height:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.height}
+                    </div>
+                  </div>
+                  <div className="expanded-data-item weight">
+                    <div className="edi-attribute">
+                      Weight:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.weight}
+                    </div>
+                  </div>
+                  <div className="expanded-data-item dob">
+                    <div className="edi-attribute">
+                      DOB:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.dob}
+                    </div>
+                  </div>
+                  <div className="expanded-data-item prior-to-nba">
+                    <div className="edi-attribute">
+                      Prior to NBA:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.prior_to_nba}
+                    </div>
+                  </div>
+                  <div className="expanded-data-item country">
+                    <div className="edi-attribute">
+                      Country:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.country}
+                    </div>
+                  </div>
+                  <div className="expanded-data-item years-in-nba">
+                    <div className="edi-attribute">
+                      Years in NBA:
+                    </div>
+                    <div className="edi-attribute-data">
+                      {player.years_pro}
+                    </div>
+                  </div>
+                </div>
+                <div className="player-stats-container">
+                  <div className="player-stats-item games">
+                    <div className="player-stats-item-attribute">
+                      GAMES
+                    </div>
+                    <div className="player-stats-item-data">
+                      {player.games_played}
+                    </div>
+                  </div>
+                  <div className="player-stats-item ppg">
+                    <div className="player-stats-item-attribute">
+                      PPG
+                    </div>
+                    <div className="player-stats-item-data">
+                      {player.ppg}
+                    </div>
+                  </div>
+                  <div className="player-stats-item rpg">
+                    <div className="player-stats-item-attribute">
+                      RPG
+                    </div>
+                    <div className="player-stats-item-data">
+                      {player.rpg}
+                    </div>
+                  </div>
+                  <div className="player-stats-item games apg">
+                    <div className="player-stats-item-attribute">
+                      APG
+                    </div>
+                    <div className="player-stats-item-data">
+                      {player.apg}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="simple-data-container">
-              <div className="name-position-container">
-                <div className="name">
-                  {player.name}
-                </div>
-                <div className="position">
-                  {player.position}
-                </div>
-              </div>
-              <div className="jersey-number">
-                {player.jersey_number}
-              </div>
-            </div>
-            <div className="expanded-data-container hide">
-              <div className="bio-container">
-                <div className="expanded-data-item height">
-                  <div className="edi-attribute">
-                    Height:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.height}
-                  </div>
-                </div>
-                <div className="expanded-data-item weight">
-                  <div className="edi-attribute">
-                    Weight:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.weight}
-                  </div>
-                </div>
-                <div className="expanded-data-item dob">
-                  <div className="edi-attribute">
-                    DOB:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.dob}
-                  </div>
-                </div>
-                <div className="expanded-data-item prior-to-nba">
-                  <div className="edi-attribute">
-                    Prior to NBA:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.prior_to_nba}
-                  </div>
-                </div>
-                <div className="expanded-data-item country">
-                  <div className="edi-attribute">
-                    Country:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.country}
-                  </div>
-                </div>
-                <div className="expanded-data-item years-in-nba">
-                  <div className="edi-attribute">
-                    Years in NBA:
-                  </div>
-                  <div className="edi-attribute-data">
-                    {player.years_pro}
-                  </div>
-                </div>
-              </div>
-              <div className="player-stats-container">
-                <div className="player-stats-item games">
-                  <div className="player-stats-item-attribute">
-                    GAMES
-                  </div>
-                  <div className="player-stats-item-data">
-                    {player.games_played}
-                  </div>
-                </div>
-                <div className="player-stats-item ppg">
-                  <div className="player-stats-item-attribute">
-                    PPG
-                  </div>
-                  <div className="player-stats-item-data">
-                    {player.ppg}
-                  </div>
-                </div>
-                <div className="player-stats-item rpg">
-                  <div className="player-stats-item-attribute">
-                    RPG
-                  </div>
-                  <div className="player-stats-item-data">
-                    {player.rpg}
-                  </div>
-                </div>
-                <div className="player-stats-item games apg">
-                  <div className="player-stats-item-attribute">
-                    APG
-                  </div>
-                  <div className="player-stats-item-data">
-                    {player.apg}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     )
   }
